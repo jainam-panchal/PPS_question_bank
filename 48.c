ab6c41aec6e6aede971e80ebcf6cbf3e7dcad0ea
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2022-03-19 07:36:39 +0530

    Update 48.c

--- 48.c
+++ 48.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+void main()
+{
+   int i,n,sum=0;
 
-int main(){
-	int i,end;
-	printf("Enter Value of N: ");
-	scanf("%d",&end);
-
-	printf("\nOdd Numbers: ");
-	for(i=1;i<=end;++i){
-		if(i%2 != 0){
-			printf("%d ",i);
-		}
-	}
-	return 0;
+   printf("Enter number of terms : ");
+   scanf("%d",&n);
+   
+   printf("\nThe odd numbers are :");
+   for(i=1;i<=n;i++)
+   {
+     printf("%d ",2*i-1);
+     sum+=2*i-1;
+   }
+   printf("\nSum of %d N terms : %d \n",n,sum);
 }
