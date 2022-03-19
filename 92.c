4a20d984c4206bf4cffbbe31329567e873e81cd9
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2022-03-19 06:21:29 +0530

    Add files via upload

---
+++ 92.c
@@ -0,0 +1,26 @@
+#include <graphics.h>
+#include <stdio.h>
+
+// driver code
+int main()
+{
+  
+  int gd = DETECT, gm, color;
+  initgraph(&gd, &gm, "");
+
+  putpixel(85, 35, GREEN);
+  putpixel(30, 40, RED);
+  putpixel(115, 50, YELLOW);
+  putpixel(135, 50, CYAN);
+  putpixel(45, 60, BLUE);
+  putpixel(20, 100, WHITE);
+  putpixel(200, 100, LIGHTBLUE);
+  putpixel(150, 100, LIGHTGREEN);
+  putpixel(200, 50, YELLOW);
+  putpixel(120, 70, RED);
+
+  getch();
+  closegraph();
+
+  return 0;
+}
